c558781af818925eaa9e80e6e8e55efa13a06d54
Abdullah <abd.ali@mail.utoronto.ca>
2017-08-03 15:33:42 -0400

    base csp model file added

---
+++ stocks_csp.py
@@ -0,0 +1,23 @@
+import sys
+
+'''
+Construct and return Mutual Funds CSP model.
+'''
+
+# take in user input, that specifies the constraints they want
+# generate satisfying tuples based on said constraints
+
+def green_constraint(csp_data):
+  return 0
+
+# more contraints here
+
+
+def mutual_funds_csp_model(csp_data):
+  '''Returns a CSP object representing a Stocks CSP problem along with an array
+  of variables for the problem.
+  '''
+
+  # actual csp model here
+  return 0
+
